ecf1f9b08355d7c9d24ada71cb19b4b4135b2396
anthony flor velasquez <antony9015@gmail.com>
2021-12-19 19:43:40 -0500

    puertas y botones

--- Assets/SampleScenes/Scripts/AbriendoPuerta.cs
+++ Assets/SampleScenes/Scripts/AbriendoPuerta.cs
@@ -7,9 +7,15 @@ public class AbriendoPuerta : MonoBehaviour
     public Animator laPuerta;
     private void OnTriggerEnter(Collider other){
         laPuerta.Play("TerrenoAbrir");
+        laPuerta.Play("AguaAbrir");
+        laPuerta.Play("TerrenoAbrir2");
+        laPuerta.Play("AguaAbrir2");
     }
 
     private void OnTriggerExit(Collider other){
         laPuerta.Play("TerrenoCerrar");
+        laPuerta.Play("AguaCerrar");
+        laPuerta.Play("TerrenoCerrar2");
+        laPuerta.Play("AguaCerrar2");
     }
 }
